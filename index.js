7c44cedc3ad968bef30a470b3b8ae76a8ba3f3bf
Oswaldo Rodriguez <rodriosw13@gmail.com>
2024-12-14 11:49:19 -0400

    Trabajando en la funcionalidad

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const nameInput = document.querySelector('.form__name-input');
 const pagesInput = document.querySelector('.form__pages-input');
 const authorInput = document.querySelector('.form__author-input');
 const formBtn = document.querySelector('.form__btn');
-const bookCard = document.querySelector('.book__card')
+const bookCard = document.querySelector('.book__card');
 
 const myLibrary = [];
 
@@ -20,6 +20,9 @@ function addBookToLibrary(name, pages, author) {
    myLibrary.push(Book(name, pages, author));
 }
 
+function removeBook(book) {
+  console.log(book);
+}
 
 formulario.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -30,26 +33,31 @@ formulario.addEventListener('submit', function(event) {
   pagesInput.value = '';
   authorInput.value = '';
 
-  console.log(myLibrary);
   const lastBook = myLibrary[myLibrary.length - 1];
 
+  const div = document.createElement('div');
+  const title = document.createElement('h3');
+  const author = document.createElement('h5');
+  const pages = document.createElement('h5');
+  const removeBtn = document.createElement('button');
+
+  div.classList.add('card');
+  removeBtn.classList.add('btn');
+
+  title.textContent = `Title: ${lastBook.name}`;
+  author.textContent = `Author: ${lastBook.author}`;
+  pages.textContent = `Pages: ${lastBook.pages}`;
+  removeBtn.textContent = 'Remove';
+
+
+  div.append(title, author, pages, removeBtn);
+  bookCard.append(div);
   
-   
-    const div = document.createElement('div');
-    const title = document.createElement('h3');
-    const author = document.createElement('h5');
-    const pages = document.createElement('h5');
-
-
-    title.textContent = `Title: ${lastBook.name}`;
-    author.textContent = `Author: ${lastBook.author}`;
-    pages.textContent = `Pages: ${lastBook.pages}`;
-
-    div.classList.add('card');
-
-    div.append(title, author, pages);
-    bookCard.append(div);
-
+  removeBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    const parent = event.target.parentNode;
+    parent.remove();
+   });
   });
 
 
